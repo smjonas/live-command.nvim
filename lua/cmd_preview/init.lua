e1b41f821b0f7ef762a33decff416228e61103d4
smjonas <jonas.strittmatter@gmx.de>
2022-07-12 13:23:50 +0200

    First draft of highlighting logic

--- lua/cmd_preview/init.lua
+++ lua/cmd_preview/init.lua
@@ -1,13 +1,12 @@
 local M = {}
 
+local config
 M.default_config = {
   cmd_name = "Norm",
   hl_group = "Substitute",
 }
 
-local scratch_buf = nil
-local cached_lines = nil
-local err = nil
+local scratch_buf, cached_lines, err
 
 local function set_error(msg, level)
   err = { msg = msg, level = level }
@@ -15,17 +14,17 @@ end
 
 -- Returns a list of insertion and replacement operations
 -- that turn the first string into the second one.
-local function levenshtein_edits(str_a, str_b)
+local function get_levenshtein_edits(str_a, str_b)
   local len_a = #str_a
   local len_b = #str_b
 
   -- quick cut-offs to save time
   if len_a == 0 then
-    return len_b
+    return {}
   elseif len_b == 0 then
-    return len_a
+    return {}
   elseif str_a == str_b then
-    return 0
+    return {}
   end
 
   local matrix = {}
@@ -44,8 +43,7 @@ local function levenshtein_edits(str_a, str_b)
   for row = 1, len_a do
     for col = 1, len_b do
       cost = str_a:sub(row, row) == str_b:sub(col, col) and 0 or 1
-      matrix[row][col] =
-        min(matrix[row - 1][col] + 1, matrix[row][col - 1] + 1, matrix[row - 1][col - 1] + cost)
+      matrix[row][col] = min(matrix[row - 1][col] + 1, matrix[row][col - 1] + 1, matrix[row - 1][col - 1] + cost)
     end
   end
 
@@ -58,24 +56,18 @@ local function levenshtein_edits(str_a, str_b)
     print(row, col)
     local edit_type
     if str_a:sub(row, row) ~= str_b:sub(col, col) then
-      if
-        matrix[row][col - 1] <= matrix[row - 1][col]
-        and matrix[row][col - 1] <= matrix[row - 1][col - 1]
-      then
-          -- Prioritize insertions and replacements
+      -- TODO: check for deletion first
+      if matrix[row][col - 1] <= matrix[row - 1][col] and matrix[row][col - 1] <= matrix[row - 1][col - 1] then
         edit_type = "insertion"
-      elseif
-        matrix[row - 1][col - 1] <= matrix[row][col]
-        and matrix[row - 1][col - 1] <= matrix[row - 1][col]
-      then
+      elseif matrix[row - 1][col - 1] <= matrix[row][col] and matrix[row - 1][col - 1] <= matrix[row - 1][col] then
         edit_type = "replacement"
         row = row - 1
       else
         print("Deletion", str_a:sub(row, row), str_b:sub(col, col))
       end
 
-      if cur_edit.type == edit_type then
-        -- Extend the start index of the edit
+      if cur_edit.type == edit_type and cur_edit.start_idx == col + 1 then
+        -- Extend the edit
         cur_edit.start_idx = col
       else
         cur_edit = { type = edit_type, start_idx = col, end_idx = col }
@@ -87,13 +79,13 @@ local function levenshtein_edits(str_a, str_b)
     col = col - 1
   end
 
-  print(vim.inspect(matrix))
-  print(vim.inspect(edits))
-  return matrix
+  -- print(vim.inspect(matrix))
+  -- print(vim.inspect(edits))
+  return edits
 end
 
 -- Called when the user is still typing the command or the command arguments
-local function incremental_norm_preview(opts, preview_ns, preview_buf)
+local function command_preview(opts, preview_ns, preview_buf)
   vim.v.errmsg = ""
   -- set_error("args:" .. opts.args .. " range start:" .. opts.line1 .. " range end:" .. opts.line2)
   -- set_error(("%d,%dnorm %s"):format(opts.line1, opts.line2, opts.args))
@@ -105,85 +97,120 @@ local function incremental_norm_preview(opts, preview_ns, preview_buf)
   local bufnr = vim.api.nvim_get_current_buf()
   if not scratch_buf then
     scratch_buf = vim.api.nvim_create_buf(true, true)
-    cached_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    cached_lines = vim.api.nvim_buf_get_lines(bufnr, opts.line1 - 1, opts.line2, false)
   else
     -- Clear the scratch buffer
     vim.api.nvim_buf_set_lines(scratch_buf, 0, -1, false, cached_lines)
   end
 
+  local range = opts.range == 2 and ("%s,%s"):format(opts.line1, opts.line2)
+    or opts.range == 1 and string(opts.line1)
+    or ""
+
   local cmd = {
     cmd = "bufdo",
-    -- args = { ("%d,%dnorm %s"):format(opts.line1, opts.line2, opts.args) },
-    args = { ("%d,%dnorm"):format(opts.line1, opts.line2), opts.args },
+    args = { ("%s%s %s"):format(range, opts.cmd, opts.args) },
     range = { scratch_buf },
   }
   -- Run the normal mode command and get the updated buffer contents
   vim.api.nvim_cmd(cmd, {})
-  local updated_lines = vim.api.nvim_buf_get_lines(scratch_buf, 0, -1, false) or {}
+  local updated_lines = vim.api.nvim_buf_get_lines(scratch_buf, opts.line1 - 1, opts.line2, false) or {}
+
+  -- Return the 0-indexed line numbers
+  local string_idx_to_pos = function(s, idx)
+    local line = 1
+    local cur_idx = 0
+    for i = 1, idx do
+      if s:sub(i, i) == "\n" then
+        line = line + 1
+        cur_idx = i
+      end
+    end
+    return line - 1, idx - cur_idx
+  end
+
   local a = table.concat(cached_lines, "\n")
   local b = table.concat(updated_lines, "\n")
+  local edits = get_levenshtein_edits(a, b)
+  set_error(vim.inspect(edits))
 
   -- Update the original buffer
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, updated_lines)
   vim.api.nvim_set_current_buf(bufnr)
-  set_error(vim.diff(a, b, { algorithm = "minimal" }))
+  vim.api.nvim_buf_set_lines(bufnr, opts.line1 - 1, opts.line2, false, updated_lines)
+
+  for _, edit in ipairs(edits) do
+    local start_line, start_col = string_idx_to_pos(b, edit.start_idx)
+    local end_line, end_col = string_idx_to_pos(b, edit.end_idx)
+    if start_line ~= end_line then
+      for line = start_line, end_line do
+        if line == start_line then
+          -- Highlight to the end of the line
+          vim.api.nvim_buf_add_highlight(bufnr, preview_ns, "Substitute", line, start_col - 1, -1)
+        elseif line == end_line then
+          -- Highlight from the start of the line
+          vim.api.nvim_buf_add_highlight(bufnr, preview_ns, "Substitute", line, 0, start_col)
+        else
+          -- Highlight the whole line
+          vim.api.nvim_buf_add_highlight(bufnr, preview_ns, "Substitute", line, 0, -1)
+        end
+      end
+    else
+      vim.api.nvim_buf_add_highlight(bufnr, preview_ns, "Substitute", start_line, start_col - 1, end_col)
+    end
+  end
   return 2
 end
 
--- Called when the command is executed (user pressed enter)
-local function incremental_norm_execute(new_name)
-  local cmd = {
-    cmd = "normal",
-    args = { "akek" },
-  }
-  -- set_error(vim.inspect(cmd))
-  cmd.count = nil
-  cmd.reg = nil
-  -- vim.api.nvim_cmd(cmd, {})
-  -- vim.api.nvim_cmd(cmd, {})
-  -- Schedule wrapping here avoids an (uncommon) issue where buffer contents were
-  -- changed by the highlight function after the rename request had already been executed.
-  -- (Probably because synchronous LSP requests are not queued like Nvim API calls?)
-  vim.schedule(function()
-    -- Any errors that occur in the preview function are not directly shown to the user but are stored in vim.v.errmsg.
-    -- For more info, see https://github.com/neovim/neovim/issues/18910.
-    if vim.v.errmsg ~= "" then
-      vim.notify(
-        "[inc-rename] An error occurred in the preview function. Please report this error here: https://github.com/smjonas/inc-rename.nvim/issues:\n"
-          .. vim.v.errmsg,
-        vim.lsp.log_levels.ERROR
-      )
-    elseif err then
-      vim.notify(err.msg, err.level)
-    else
-      -- perform_lsp_rename(new_name)
-    end
-  end)
+local function execute_command(command)
+  -- vim.schedule(function()
+  -- Any errors that occur in the preview function are not directly shown to the user but stored in vim.v.errmsg.
+  -- For more info, see https://github.com/neovim/neovim/issues/18910.
+  if vim.v.errmsg ~= "" then
+    vim.notify(
+      "[command-preview] An error occurred in the preview function. Please report this error here: https://github.com/smjonas/command-preview.nvim/issues:\n"
+        .. vim.v.errmsg,
+      vim.lsp.log_levels.ERROR
+    )
+  elseif err then
+    vim.notify(err.msg, err.level)
+  else
+    print(command)
+    vim.cmd(command)
+    vim.api.nvim_buf_delete(scratch_buf, {})
+    scratch_buf = nil
+  end
+  -- end)
 end
 
-local create_user_command = function(cmd_name)
-  vim.api.nvim_create_user_command(cmd_name, function(opts)
-    incremental_norm_execute(opts.args)
-    -- TODO: range = true?
-  end, {
-    nargs = "+",
-    range = true,
-    addr = "lines",
-    preview = incremental_norm_preview,
-  })
+local create_user_commands = function(commands)
+  for name, command in pairs(commands) do
+    vim.api.nvim_create_user_command(name, function(opts)
+      local range = opts.range == 2 and ("%s,%s"):format(opts.line1, opts.line2)
+        or opts.range == 1 and string(opts.line1)
+        or ""
+      execute_command(("%s%s %s"):format(range, command.cmd, opts.args))
+    end, {
+      nargs = "*",
+      range = true,
+      preview = function(opts, preview_ns, preview_buf)
+        opts.cmd = command.cmd
+        return command_preview(opts, preview_ns, preview_buf)
+      end,
+    })
+  end
 end
 
 M.setup = function(user_config)
   if vim.fn.has("nvim-0.8.0") ~= 1 then
     vim.notify(
-      "[inc-norm] This plugin requires Neovim nightly (0.8). Please upgrade your Neovim version.",
+      "[command-preview] This plugin requires Neovim nightly (0.8). Please upgrade your Neovim version.",
       vim.lsp.log_levels.ERROR
     )
     return
   end
 
-  M.config = vim.tbl_deep_extend("force", M.default_config, user_config or {})
-  create_user_command(M.config.cmd_name)
+  config = vim.tbl_deep_extend("force", M.default_config, user_config or {})
+  create_user_commands(config.commands)
 end
 
 return M
