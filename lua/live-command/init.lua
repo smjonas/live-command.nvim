958e6887332a9cc3e508ea6f025f9af7f69ff1ed
smjonas <jonas.strittmatter@gmx.de>
2022-10-12 11:44:21 +0200

    stash

--- lua/live-command/init.lua
+++ lua/live-command/init.lua
@@ -1,18 +1,134 @@
 local M = {}
 
-M.defaults = {
-  enable_highlighting = true,
-  inline_highlighting = true,
-  hl_groups = {
-    insertion = "DiffAdd",
-    deletion = "DiffDelete",
-    change = "DiffChange",
-  },
-}
-
 local should_cache_lines = true
-local cached_lines
-local prev_lazyredraw
+local cached_lines, updated_lines
+
+local function get_highlights(line_range)
+  local highlights = {}
+  -- Using the on_hunk callback and returning -1 to cancel causes an error so don't use that
+  local hunks = vim.diff(table.concat(cached_lines, "\n"), table.concat(updated_lines, "\n"), {
+    result_type = "indices",
+  })
+  log(("Visible line range: %d-%d"):format(line_range[1], line_range[2]))
+
+  for i, hunk in ipairs(hunks) do
+    log(function()
+      return ("Hunk %d/%d: %s"):format(i, #hunks, vim.inspect(hunk))
+    end)
+
+    local start_a, count_a, start_b, count_b = hunk[1], hunk[2], hunk[3], hunk[4]
+    local hunk_kind = (count_a < count_b and "insertion") or (count_a > count_b and "deletion")
+    if hunk_kind then
+      local start_line, end_line
+      if hunk_kind == "insertion" then
+        start_line = start_b + count_a
+        end_line = start_a + (count_b - count_a)
+      else
+        start_line = start_a + count_b
+        end_line = start_line + (count_a - count_b) - 1
+      end
+
+      log(function()
+        return ("Lines %d-%d:\nOriginal: %s\nUpdated: %s"):format(
+          start_line,
+          end_line,
+          vim.inspect(vim.list_slice(cached_lines, start_line, end_line)),
+          vim.inspect(vim.list_slice(updated_lines, start_line, end_line))
+        )
+      end)
+
+      for line = start_line, end_line do
+        -- Outside of visible area, skip current or all hunks
+        if line > line_range[2] then
+          return highlights
+        end
+
+        if line >= line_range[1] then
+          if updated_lines[line] == "" then
+            -- Make empty lines visible
+            updated_lines[line] = " "
+          end
+          table.insert(highlights, { kind = hunk_kind, line = line, column = 1, length = -1 })
+        end
+      end
+    else
+      -- Change edit
+      for line = start_b, start_b + count_b - 1 do
+        -- Outside of visible area, skip current or all hunks
+        if line > line_range[2] then
+          return highlights
+        end
+
+        if line >= line_range[1] then
+          if opts.inline_highlighting then
+            -- Get diff for each line in the hunk
+            add_inline_highlights(line, cached_lines, updated_lines, opts.undo_deletions, highlights)
+          else
+            -- Use a single highlight for the whole line
+            table.insert(highlights, { kind = "change", line = line, column = 1, length = -1 })
+          end
+        end
+      end
+    end
+  end
+  return highlights
+end
+
+local function command_preview(command, opts, preview_ns, preview_buf)
+  local bufnr = vim.api.nvim_get_current_buf()
+  if should_cache_lines then
+    cached_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    should_cache_lines = false
+  end
+  local visible_line_range = { vim.fn.line("w0"), vim.fn.line("w$") }
+  command(opts)
+  updated_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+
+  -- Adjust range to account for potentially inserted lines
+  visible_line_range = {
+    math.max(visible_line_range[1], vim.fn.line("w0")),
+    math.max(visible_line_range[2], vim.fn.line("w$")),
+  }
+
+  -- An empty buffer is represented as { "" }, change it to {}
+  if not updated_lines[2] and updated_lines[1] == "" then
+    updated_lines = {}
+  end
+
+  local highlights = get_highlights(visible_line_range)
+  log(function()
+    return "Highlights: " .. vim.inspect(highlights)
+  end)
+
+  for _, hl in ipairs(highlights) do
+    local hl_group = command.hl_groups[hl.kind]
+    if hl_group ~= false then
+      -- Convert 1-based to 0-based
+      vim.api.nvim_buf_add_highlight(
+        bufnr,
+        preview_ns,
+        hl_group,
+        hl.line - 1,
+        hl.column - 1,
+        hl.length == -1 and -1 or hl.column + hl.length - 1
+      )
+    end
+  end
+  return 2
+end
+
+local command = function(opts)
+  vim.cmd("norm Ikek")
+end
+
+vim.api.nvim_create_user_command("Test", command, {
+  preview = function(opts, preview_ns, preview_buf)
+    return command_preview(command, opts, preview_ns, preview_buf)
+  end,
+  nargs = 0,
+})
+
+-- If preview is a boolean, use command_preview function and pass callback to preview function
 
 local logs = {}
 local function log(msg, level)
@@ -107,280 +223,16 @@ end
 -- Expose function to tests
 M._add_inline_highlights = add_inline_highlights
 
-local function get_diff_highlights(cached_lns, updated_lines, line_range, opts)
-  local highlights = {}
-  -- Using the on_hunk callback and returning -1 to cancel causes an error so don't use that
-  local hunks = vim.diff(table.concat(cached_lns, "\n"), table.concat(updated_lines, "\n"), {
-    result_type = "indices",
-  })
-  log(("Visible line range: %d-%d"):format(line_range[1], line_range[2]))
-
-  for i, hunk in ipairs(hunks) do
-    log(function()
-      return ("Hunk %d/%d: %s"):format(i, #hunks, vim.inspect(hunk))
-    end)
-
-    local start_a, count_a, start_b, count_b = hunk[1], hunk[2], hunk[3], hunk[4]
-    local hunk_kind = (count_a < count_b and "insertion") or (count_a > count_b and "deletion")
-    if hunk_kind then
-      local start_line, end_line
-      if hunk_kind == "insertion" then
-        start_line = start_b + count_a
-        end_line = start_a + (count_b - count_a)
-      else
-        start_line = start_a + count_b
-        end_line = start_line + (count_a - count_b) - 1
-      end
-
-      log(function()
-        return ("Lines %d-%d:\nOriginal: %s\nUpdated: %s"):format(
-          start_line,
-          end_line,
-          vim.inspect(vim.list_slice(cached_lns, start_line, end_line)),
-          vim.inspect(vim.list_slice(updated_lines, start_line, end_line))
-        )
-      end)
-
-      for line = start_line, end_line do
-        -- Outside of visible area, skip current or all hunks
-        if line > line_range[2] then
-          return highlights
-        end
-
-        if line >= line_range[1] then
-          if hunk_kind == "deletion" and opts.undo_deletions then
-            -- Hunk was deleted: reinsert lines
-            table.insert(updated_lines, line, cached_lns[line])
-          end
-          if updated_lines[line] == "" then
-            -- Make empty lines visible
-            updated_lines[line] = " "
-          end
-          table.insert(highlights, { kind = hunk_kind, line = line, column = 1, length = -1 })
-        end
-      end
-    else
-      -- Change edit
-      for line = start_b, start_b + count_b - 1 do
-        -- Outside of visible area, skip current or all hunks
-        if line > line_range[2] then
-          return highlights
-        end
-
-        if line >= line_range[1] then
-          if opts.inline_highlighting then
-            -- Get diff for each line in the hunk
-            add_inline_highlights(line, cached_lns, updated_lines, opts.undo_deletions, highlights)
-          else
-            -- Use a single highlight for the whole line
-            table.insert(highlights, { kind = "change", line = line, column = 1, length = -1 })
-          end
-        end
-      end
-    end
-  end
-  return highlights
-end
-
--- Expose functions to tests
-M._preview_across_lines = get_diff_highlights
-
-local function run_buf_cmd(buf, cmd)
-  vim.api.nvim_buf_call(buf, function()
-    log(function()
-      return ("Previewing command: %s (current line = %d)"):format(cmd, vim.api.nvim_win_get_cursor(0)[1])
-    end)
-    vim.cmd(cmd)
-  end)
-end
-
--- Called when the user is still typing the command or the command arguments
-local function command_preview(opts, preview_ns, preview_buf)
-  -- Any errors that occur in the preview function are not directly shown to the user but stored in vim.v.errmsg.
-  -- Related: https://github.com/neovim/neovim/issues/18910.
-  vim.v.errmsg = ""
-  logs = {}
-  local args = opts.cmd_args
-  local command = opts.command
-
-  local bufnr = vim.api.nvim_get_current_buf()
-  if should_cache_lines then
-    prev_lazyredraw = vim.o.lazyredraw
-    vim.o.lazyredraw = true
-    cached_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-    should_cache_lines = false
-  end
-
-  -- Ignore any errors that occur while running the command.
-  -- This reduces noise when a plugin modifies vim.v.errmsg (whether accidentally or not).
-  local prev_errmsg = vim.v.errmsg
-  local visible_line_range = { vim.fn.line("w0"), vim.fn.line("w$") }
-
-  if opts.line1 == opts.line2 then
-    run_buf_cmd(bufnr, ("%s %s"):format(command.cmd, args))
-  else
-    run_buf_cmd(bufnr, ("%d,%d%s %s"):format(opts.line1, opts.line2, command.cmd, args))
-  end
-
-  vim.v.errmsg = prev_errmsg
-  local updated_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  visible_line_range[2] = math.max(visible_line_range[2], vim.fn.line("w$"))
-  -- Adjust end to account for potentially newly inserted lines
-  visible_line_range[2] = visible_line_range[2] + math.max(visible_line_range[2], vim.fn.line("w$"))
-
-  local set_lines = function(lines)
-    -- TODO: is this worth optimizing?
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-    if preview_buf then
-      vim.api.nvim_buf_set_lines(preview_buf, 0, -1, false, lines)
-    end
-  end
-
-  if not opts.line1 or not command.enable_highlighting then
-    set_lines(updated_lines)
-    -- This should not happen
-    if not opts.line1 then
-      log("No line1 range provided", "ERROR")
-    end
-    return 2
-  end
-
-  -- An empty buffer is represented as { "" }, change it to {}
-  if not updated_lines[2] and updated_lines[1] == "" then
-    updated_lines = {}
-  end
-
-  local highlights = get_diff_highlights(cached_lines, updated_lines, visible_line_range, {
-    undo_deletions = command.hl_groups["deletion"] ~= false,
-    inline_highlighting = command.inline_highlighting,
-  })
-  log(function()
-    return "Highlights: " .. vim.inspect(highlights)
-  end)
-
-  set_lines(updated_lines)
-  for _, hl in ipairs(highlights) do
-    local hl_group = command.hl_groups[hl.kind]
-    if hl_group ~= false then
-      vim.api.nvim_buf_add_highlight(
-        bufnr,
-        preview_ns,
-        hl_group,
-        hl.line - 1,
-        hl.column - 1,
-        hl.length == -1 and -1 or hl.column + hl.length - 1
-      )
-    end
-  end
-  return 2
-end
-
-local function restore_buffer_state()
-  vim.o.lazyredraw = prev_lazyredraw
-  should_cache_lines = true
-  if vim.v.errmsg ~= "" then
-    log(("An error occurred in the preview function:\n%s"):format(vim.inspect(vim.v.errmsg)), "ERROR")
-  end
-end
-
-local function execute_command(command)
-  log("Executing command: " .. command)
-  vim.cmd(command)
-  restore_buffer_state()
-end
-
-local create_user_commands = function(commands)
-  for name, command in pairs(commands) do
-    local args, range
-    vim.api.nvim_create_user_command(name, function(opts)
-      local range_string = range and range
-        or (
-          opts.range == 2 and ("%s,%s"):format(opts.line1, opts.line2)
-          or opts.range == 1 and tostring(opts.line1)
-          or ""
-        )
-      execute_command(("%s%s %s"):format(range_string, command.cmd, args))
-    end, {
-      nargs = "*",
-      range = true,
-      preview = function(opts, preview_ns, preview_buf)
-        opts.command = command
-        args = command.args
-        if args then
-          -- Update command args if provided
-          args = type(args) == "function" and args(opts) or args
-        else
-          args = opts.args
-        end
-        opts.cmd_args = args
-        range = command.range
-        return command_preview(opts, preview_ns, preview_buf)
-      end,
-    })
-  end
-end
-
-local validate_config = function(config)
-  local defaults = config.defaults
-  vim.validate {
-    defaults = { defaults, "table", true },
-    commands = { config.commands, "table" },
-  }
-  local possible_opts = { "enable_highlighting", "inline_highlighting", "hl_groups" }
-  for _, command in pairs(config.commands) do
-    for _, opt in ipairs(possible_opts) do
-      if command[opt] == nil and defaults and defaults[opt] ~= nil then
-        command[opt] = defaults[opt]
-      else
-        command[opt] = command[opt] or M.defaults[opt]
-      end
-    end
-    command.hl_groups = vim.tbl_deep_extend("force", {}, M.defaults.hl_groups, command.hl_groups)
-
-    vim.validate {
-      cmd = { command.cmd, "string" },
-      args = { command.args, { "string", "function" }, true },
-      range = { command.range, { "string" }, true },
-      ["command.enable_highlighting"] = { command.enable_highlighting, "boolean", true },
-      ["command.inline_highlighting"] = { command.inline_highlighting, "boolean", true },
-      ["command.hl_groups"] = { command.hl_groups, "table", true },
-    }
-  end
-end
-
-M.setup = function(user_config)
-  if vim.fn.has("nvim-0.8.0") ~= 1 then
-    vim.notify(
-      "[live-command] This plugin requires at least Neovim 0.8. Please upgrade your Neovim version.",
-      vim.log.levels.ERROR
-    )
-    return
-  end
-
-  local config = vim.tbl_deep_extend("force", M.defaults, user_config or {})
-  validate_config(config)
-  create_user_commands(config.commands)
-
-  local id = vim.api.nvim_create_augroup("command_preview.nvim", { clear = true })
-  -- We need to be able to tell when the command was cancelled so the buffer lines are refetched next time.
-  vim.api.nvim_create_autocmd({ "CmdLineLeave" }, {
-    group = id,
-    -- Schedule wrap to run after a potential command execution
-    callback = vim.schedule_wrap(function()
-      restore_buffer_state()
-    end),
-  })
-
-  M.debug = user_config.debug
-
-  vim.api.nvim_create_user_command("LiveCommandLog", function()
-    local msg = ("live-command log\n================\n\n%s%s"):format(
-      logs.ERROR and "[ERROR]\n" .. table.concat(logs.ERROR, "\n") .. (logs.TRACE and "\n" or "") or "",
-      logs.TRACE and "[TRACE]\n" .. table.concat(logs.TRACE, "\n") or ""
-    )
-    vim.notify(msg)
-  end, { nargs = 0 })
-end
+-- TODO: restore_buffer_state
+local id = vim.api.nvim_create_augroup("command_preview.nvim", { clear = true })
+-- We need to be able to tell when the command was cancelled so the buffer lines are refetched next time.
+vim.api.nvim_create_autocmd({ "CmdLineLeave" }, {
+  group = id,
+  -- Schedule wrap to run after a potential command execution
+  callback = vim.schedule_wrap(function()
+    restore_buffer_state()
+  end),
+})
 
 M.version = "1.2.0"
 
