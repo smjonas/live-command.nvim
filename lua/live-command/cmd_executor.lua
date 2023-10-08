5f6a3e708295c6c3f82a0dae5f347274d0d50f0a
smjonas <jonas.strittmatter@gmx.de>
2023-10-08 20:00:48 +0200

    Add highlighter code

--- lua/live-command/cmd_executor.lua
+++ lua/live-command/cmd_executor.lua
@@ -8,16 +8,17 @@ local running = false
 
 local execute_command = function(cmd, bufnr)
   local old_buf_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local visible_line_range = { vim.fn.line("w0"), vim.fn.line("w$") }
   vim.cmd(cmd)
-  -- Emulate slow-running command
-  for i = 1, 1000000000 do
-    old_buf_lines = old_buf_lines
-  end
+  visible_line_range = {
+    math.max(visible_line_range[1], vim.fn.line("w0")),
+    math.max(visible_line_range[2], vim.fn.line("w$")),
+  }
   local new_buf_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  return old_buf_lines, new_buf_lines
+  return old_buf_lines, new_buf_lines, visible_line_range
 end
 
----@param cmd string
+---@param cmd LiveCommand
 ---@param bufnr number
 M.submit_command = function(cmd, bufnr, on_receive_highlights)
   if cmd == latest_cmd then
@@ -26,9 +27,9 @@ M.submit_command = function(cmd, bufnr, on_receive_highlights)
   latest_cmd = cmd
   if not running then
     running = true
-    local old_buf_lines, new_buf_lines = execute_command(cmd, bufnr)
+    local old_buf_lines, new_buf_lines, visible_line_range = execute_command(cmd, bufnr)
     local diff = differ.get_diff(old_buf_lines, new_buf_lines)
-    local highlights = highlighter.get_highlights(diff)
+    local highlights = highlighter.get_highlights(diff, old_buf_lines, new_buf_lines, p)
     highlights = cmd
     on_receive_highlights(highlights, bufnr)
     running = false
