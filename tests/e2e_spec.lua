2ed5c5058160410d15d32ec1fa7d364d294dfab5
smjonas <jonas.strittmatter@gmx.de>
2024-09-15 13:33:32 +0200

    test: fix failing e2e tests

--- tests/e2e_spec.lua
+++ tests/e2e_spec.lua
@@ -2,55 +2,44 @@ local live_command = require("live-command")
 local api = vim.api
 
 local bufnr
-local create_preview_cmd = function(cmd_name, cmd_opts)
-  api.nvim_set_current_buf(bufnr)
-  live_command.create_previewable_command(cmd_name, cmd_opts)
-end
-
-local preview = function(cmd)
-  api.nvim_set_current_buf(bufnr)
-  vim.cmd("Preview " .. cmd)
-end
 
 local get_lines = function()
   return api.nvim_buf_get_lines(bufnr, 0, -1, false)
 end
 
-setup(function()
-  live_command.setup()
-end)
-
 before_each(function()
   bufnr = api.nvim_create_buf(false, true)
   api.nvim_buf_set_lines(bufnr, 0, -1, false, { "First line", "Second line" })
+  api.nvim_set_current_buf(bufnr)
 end)
 
 describe("create_preview_command works for", function()
   it("norm command", function()
-    create_preview_cmd("Norm", { cmd = "norm" })
+    live_command.create_previewable_command("Norm", { cmd = "norm" })
     vim.cmd("Norm daw")
     assert.are_same({ "line", "Second line" }, get_lines())
   end)
 
   it("norm command with count", function()
-    create_preview_cmd("Norm", { cmd = "norm" })
+    live_command.create_previewable_command("Norm", { cmd = "norm" })
     vim.cmd("2Norm daw")
     assert.are_same({ "First line", "line" }, get_lines())
   end)
 
   it("norm command with range", function()
-    create_preview_cmd("Norm", { cmd = "norm" })
+    live_command.create_previewable_command("Norm", { cmd = "norm" })
     vim.cmd("1,2Norm daw")
     assert.are_same({ "line", "line" }, get_lines())
   end)
 
   it("g command", function()
-    create_preview_cmd("G", { cmd = "g" })
+    live_command.create_previewable_command("G", { cmd = "g" })
     vim.cmd("G/Second/d")
     assert.are_same({ "First line" }, get_lines())
   end)
 
-  it("command spec in config", function()
+  it("#kek command spec in config", function()
+    vim.api.nvim_set_current_buf(bufnr)
     live_command.setup {
       commands = {
         ABC = { cmd = "norm" },
@@ -63,20 +52,17 @@ end)
 
 describe(":Preview works for", function()
   it("norm command", function()
-    local cmd = "norm daw"
-    preview(cmd)
+    vim.cmd("Preview norm daw")
     assert.are_same({ "line", "Second line" }, get_lines())
   end)
 
   it("norm command with count", function()
-    local cmd = "2norm daw"
-    preview(cmd)
+    vim.cmd("Preview 2norm daw")
     assert.are_same({ "First line", "line" }, get_lines())
   end)
 
   it("g command", function()
-    local cmd = "g/Second/d"
-    preview(cmd)
+    vim.cmd("Preview g/Second/d")
     assert.are_same({ "First line" }, get_lines())
   end)
 end)
